54ca64584049bee023c93a52ea8a08a9c265878e
clemdt <clementduboistaine@gmail.com>
2023-12-15 14:19:08 +0100

    remove AdressExcpetion AdressRepository AdressService

--- src/main/java/eshop/exceptions/AdressException.java
+++
@@ -1,7 +0,0 @@
-package eshop.exceptions;
-
-public class AdressException extends RuntimeException {
-	public AdressException(String message) {
-		super(message);
-	}
-}
--- src/main/java/eshop/repositories/AdressRepository.java
+++
@@ -1,8 +0,0 @@
-package eshop.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import eshop.model.Adress;
-
-public interface AdressRepository extends JpaRepository<Adress, Long> {
-}
